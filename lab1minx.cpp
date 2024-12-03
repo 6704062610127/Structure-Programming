e26ea31166f1794273e00c1aa36055c13e7c9dfa
6704062610127 <s6704062610127@email.kmutnb.ac.th>
2024-12-03 13:23:37 +0700

    ข้อ 3

--- lab1minx.cpp
+++ lab1minx.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
-int main() {
-	int var1 = 20;
-	int var2 = 10;
-	if(var1>var2){
-		printf("var1 is greater than var2");
+int main(){
+	int n1, n2;
+	printf("Please enter n1 and n2 : ");
+	scanf("%d %d", &n1, &n2);
+	    if (n1 > n2){
+	printf("number1 is greater than number2 = %d\n", n1 - n2);
+	}else{
+	printf("number2 is greater than number1 = %d\n", n2 - n1);
 	}
+	return 0;
 }
